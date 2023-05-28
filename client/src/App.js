ebae06b4ab70eac35048eee55153c0d122b31a8e
Mari Azh <Marzi.aghchay@gmail.com>
2023-05-28 14:04:03 +0100

    router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,51 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Single from "./pages/Single";
+import Write from "./pages/Write";
+import Footer from "./componets/Footer";
+import Navbar from "./componets/Navbar";
+
+
+const Layout = () => {
+return(
+  <>
+  <Navbar/>
+  <Outlet/>
+  <Footer/>
+  </>
+)
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/post/:id",
+        element:<Single />
+      },
+      {
+        path: "/write",
+        element: <Write />
+      },
+    ],
+  },
+]);
 
 
 function App() {
   return (
     <div className="App">
-     hi
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
