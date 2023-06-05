12a8709098cac3a0fc6387b55338b337186ad8ad
Mari Azh <Marzi.aghchay@gmail.com>
2023-06-05 16:57:43 +0100

    add Menu to single page and make style for menu

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import Edit from "../img/edit.png";
 import Delete from "../img/delete.png";
+import Menu from '../components/Mene';
 
 const Single = () => {
   return (
@@ -9,7 +10,7 @@ const Single = () => {
       <div className="single">
         <div className="content">
           <img
-            src="https://images.pexels.com/photos/1024967/pexels-photo-1024967.jpeg?auto=compress&cs=tinysrgb&w=300"
+            src="https://images.pexels.com/photos/2928027/pexels-photo-2928027.jpeg?cs=srgb&w=2000"
             alt=""
           />
           <div className="user">
@@ -26,25 +27,27 @@ const Single = () => {
             </Link>
             <img src={Delete} alt="" />
           </div>
+          <h1>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis culpa
+            libero laboriosam repellat facere voluptate ipsam molestiae corporis
+            assumenda rerum.
+          </h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius
+            minima nulla iusto consequatur id beatae voluptates autem, dolorem
+            voluptate cum. Doloribus id dignissimos illum. Corrupti minus iure
+            doloribus eius doloremque, sequi unde nihil, ullam, praesentium
+            beatae quibusdam cumque odio officiis iste ex non adipisci mollitia
+            sapiente voluptate! Cum nesciunt recusandae sapiente placeat quae,
+            nihil quo est quos! Nostrum dolorem id voluptates optio odit
+            voluptas cumque! Cum, possimus omnis. Iure voluptatibus magni
+            exercitationem repudiandae dolor veniam consectetur similique
+            explicabo natus accusantium!
+          </p>
         </div>
-        <h1>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis culpa
-          libero laboriosam repellat facere voluptate ipsam molestiae corporis
-          assumenda rerum.
-        </h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius minima
-          nulla iusto consequatur id beatae voluptates autem, dolorem voluptate
-          cum. Doloribus id dignissimos illum. Corrupti minus iure doloribus
-          eius doloremque, sequi unde nihil, ullam, praesentium beatae quibusdam
-          cumque odio officiis iste ex non adipisci mollitia sapiente voluptate!
-          Cum nesciunt recusandae sapiente placeat quae, nihil quo est quos!
-          Nostrum dolorem id voluptates optio odit voluptas cumque! Cum,
-          possimus omnis. Iure voluptatibus magni exercitationem repudiandae
-          dolor veniam consectetur similique explicabo natus accusantium!
-        </p>
+
+        <Menu className="menu" />
       </div>
-     <div className='menu'>Menu</div>
     </div>
   );
 }
